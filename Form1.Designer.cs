87f594ee508e3d2bb8de699e815df8d2dccd2f11
veegr <veegr@DIRASHER>
2024-09-18 10:01:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace lab_5_OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(708, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(482, 37);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(220, 450);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(256, 37);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(220, 453);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Смоделировать выполнение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 173);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(149, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 85);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(151, 85);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Количество генерируемых задач(N)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Сгенерированные задачи";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Отсортированные задачи";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(708, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Процесс выполнения задач";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Центральный процессор(k+-l)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Жесткий диск(m+-v)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Сеть(b+-d)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(237, 77);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Остортировать список задач";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(237, 71);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Сгенерировать список задач";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(119, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 16);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "+-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(119, 135);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 16);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "+-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(119, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 16);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "+-";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 444);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 20);
+            this.radioButton1.TabIndex = 28;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1 мс";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(146, 444);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 20);
+            this.radioButton2.TabIndex = 29;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "100 мс";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(9, 470);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 20);
+            this.radioButton3.TabIndex = 30;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "10 мс";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(147, 470);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(75, 20);
+            this.radioButton4.TabIndex = 31;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1000 мс";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 499);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Модель";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_5_OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = N.ToString();
+            textBox2.Text = b.ToString();
+            textBox3.Text = d.ToString();
+            textBox4.Text = m.ToString();
+            textBox5.Text = v.ToString();
+            textBox6.Text = k.ToString();
+            textBox7.Text = l.ToString();
+        }
+
+        int N = 42, 
+            k = 266, 
+            l = 95, 
+            m = 277, 
+            v = 42, 
+            b = 624, 
+            d = 35;
+
+        //Объявляем блокирующие переменные глобально
+        bool cpu = true;
+        bool hdd = true;
+        bool lan = true;
+        //Время занятости устройств
+        int cpu_busy_time = 0;
+        int hdd_busy_time = 0;
+        int lan_busy_time = 0;
+        //Время бехдействия устройств
+        int cpu_stop_time = 0;
+        int hdd_stop_time = 0;
+        int lan_stop_time = 0;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int cpu_current = 0;
+            int hdd_current = 0;
+            int lan_current = 0;
+            
+            //Если свободен, ищем для него процесс
+            if (cpu)
+            {
+                for(int i = 0; i < N; i++)
+                {
+                    if (mas_task[i].cpu_time > 0) 
+                    {
+                        cpu = false; //Занимает устройство
+                        cpu_current = i;
+                        break;
+                    }
+                }
+            }
+            //Если ЦП занят, уменьшаем время обработки
+            if(!cpu && mas_task[cpu_current].cpu_time > 0)
+            {
+                mas_task[cpu_current].cpu_time --;
+                cpu_busy_time++;
+            }
+
+            //Если процесс выполнился на ЦП - освобождаем его
+            if (!cpu && mas_task[cpu_current].cpu_time <= 0)
+            {
+                cpu = true;
+            }
+
+            //Если свободен, ищем для него процесс
+            if (hdd && mas_task[cpu_current].cpu_time == 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (mas_task[i].hdd_time > 0)
+                    {
+                        hdd = false; //Занимает устройство
+                        hdd_current = i;
+                        break;
+                    }
+                }
+            }
+            //Если Hdd занят, уменьшаем время обработки
+            if (!hdd && mas_task[hdd_current].hdd_time > 0)
+            {
+                mas_task[hdd_current].hdd_time--;
+                hdd_busy_time++;
+                hdd_stop_time = cpu_busy_time;
+            }
+
+            //Если процесс выполнился на Hdd - освобождаем его
+            if (!hdd && mas_task[hdd_current].hdd_time <= 0)
+            {
+                hdd = true;
+            }
+
+            //Если свободен, ищем для него процесс
+            if (lan && mas_task[hdd_current].hdd_time == 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (mas_task[i].lan_time > 0)
+                    {
+                        lan = false; //Занимает устройство
+                        lan_current = i;
+                        break;
+                    }
+                }
+            }
+            //Если lan занят, уменьшаем время обработки
+            if (!lan && mas_task[lan_current].lan_time > 0)
+            {
+                mas_task[lan_current].lan_time--;
+                lan_busy_time++;
+            }
+
+            //Если процесс выполнился на lan - освобождаем его
+            if (!lan && mas_task[lan_current].lan_time <= 0)
+            {
+                lan = true;
+            }
+            //Выводим выполненный масив на форму
+            richTextBox2.Clear();
+            for(int i = 0;i < N;i++)
+            {
+                if (mas_task[i].id < 10)
+                {
+                    richTextBox2.Text += "0" + mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                    richTextBox1.Text = "Выполнено процессором: " + cpu_busy_time.ToString() + "\nВыполнено диском: " + hdd_busy_time.ToString() + "\nВыполнено сетью: " + lan_busy_time.ToString() + "\nОжидание процессора: " + hdd_stop_time.ToString() + "\nОжидание диска: " + hdd_stop_time.ToString() + "\nОжидание сети: " + hdd_stop_time.ToString();
+                }
+                else
+                {
+                    richTextBox2.Text += mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                    richTextBox1.Text = "Выполнено процессором: " + cpu_busy_time.ToString() + "\nВыполнено диском: " + hdd_busy_time.ToString() + "\nВыполнено сетью: " + lan_busy_time.ToString() + "\nОжидание процессора: " + hdd_stop_time.ToString() + "\nОжидание диска: " + hdd_stop_time.ToString() + "\nОжидание сети: " + hdd_stop_time.ToString();
+                }
+            }
+        }
+
+        public struct task
+        {
+            public int id;
+            public int cpu_time;
+            public int hdd_time;
+            public int lan_time;
+        }
+        task[] mas_task;
+        private void button3_Click(object sender, EventArgs e)//Сгенерировать список задач
+        {
+            //Генерируем время обработки
+            Random r = new Random();
+            mas_task = new task[N];
+            for (int i = 0; i < N; i++)
+            {
+                mas_task[i].id = i + 1;
+                mas_task[i].cpu_time = k - l + r.Next(l * 2);
+                mas_task[i].hdd_time = m - v + r.Next(v * 2);
+                mas_task[i].lan_time = b - d + r.Next(d * 2);
+            }
+            //Выводим на форму
+            richTextBox3.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                if(i < 9)
+                {
+                    richTextBox3.Text += "0" + mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                }
+                else
+                {
+                    richTextBox3.Text += mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Отсортировать список задач
+        {
+            //Сортируем список задач по загрузке ЦП
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (mas_task[i].cpu_time < mas_task[j].cpu_time)
+                    {
+                        task temp = mas_task[i];
+                        mas_task[i] = mas_task[j];
+                        mas_task[j] = temp;
+                    }
+                }
+            }
+            //Выводим отсортированный на форму
+            richTextBox2.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                if (mas_task[i].id < 10)
+                {
+                    richTextBox2.Text += "0" + mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                }
+                else
+                {
+                    richTextBox2.Text += mas_task[i].id.ToString() + " " + mas_task[i].cpu_time.ToString() + " " + mas_task[i].hdd_time.ToString() + " " + mas_task[i].lan_time.ToString() + "\n";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Смоделировать выполнение
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
